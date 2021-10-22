4b312f3dddd47b77daf4de183165f2717f2444f8
Will Chadwick <WillCDevelopment@gmail.com>
2021-10-22 20:45:04 +0100

    bootstrap fastify server (#1)
    * bootstrap fastifdy server
    
    * tiday up configs

---
+++ server/index.ts
@@ -0,0 +1,29 @@
+import fastify from 'fastify'
+import routes from './routes'
+
+export const server = fastify({
+  logger: {
+    level: 'info',
+    serializers: {
+      req(req) {
+        return {
+          method: req.method,
+          url: req.url,
+          headers: req.headers,
+          body: req.body,
+        }
+      },
+    },
+  },
+  ignoreTrailingSlash: true,
+})
+
+server.register(routes)
+
+server.listen(8080, (err, address) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Server listening at ${address}`)
+})
