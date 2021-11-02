1cdd520c7c44ade21f35618543b606dd16392c52
Will Chadwick <WillCDevelopment@gmail.com>
2021-11-02 09:37:16 +0000

    clean up React components (#16)

--- app/src/pages/SplashPage/SplashPage.tsx
+++ app/src/pages/SplashPage/SplashPage.tsx
@@ -3,24 +3,18 @@ import Button from 'core/components/Button'
 import DownloadPayrollButton from './components/DownloadPayrollButton'
 import styles from './SplashPage.less'
 
-const SplashPage: FC = () => {
-  return (
-    <div className={styles.splashpage}>
-      <header data-test-id="titleBanner">
-        <h1>WillCorp</h1>
-        <h4>Productivity Tools</h4>
-      </header>
+const SplashPage: FC = () => (
+  <div className={styles.splashpage}>
+    <header data-test-id="titleBanner">
+      <h1>WillCorp</h1>
+      <h4>Productivity Tools</h4>
+    </header>
 
-      <section>
-        <Button
-          text="Payroll Viewer"
-          style={{ width: '40%' }}
-          href="/payroll"
-        />
-        <DownloadPayrollButton />
-      </section>
-    </div>
-  )
-}
+    <section>
+      <Button text="Payroll Viewer" style={{ width: '40%' }} href="/payroll" />
+      <DownloadPayrollButton />
+    </section>
+  </div>
+)
 
 export default SplashPage
