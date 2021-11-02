8778535657443b69062087423b14271ace7d186f
Will Chadwick <willcdevelopment@gmail.com>
2021-11-02 09:35:19 +0000

    clean up React components

--- app/src/pages/PayrollPage/PayrollPage.tsx
+++ app/src/pages/PayrollPage/PayrollPage.tsx
@@ -3,20 +3,18 @@ import { Link } from 'react-router-dom'
 import PayrollTable from './containers/PayrollTable'
 import styles from './PayrollPage.less'
 
-const PayrollPage: FC = () => {
-  return (
-    <>
-      <Link to="/" className={styles.backbutton}>
-        <span>{'<'}</span>
-      </Link>
-      <div className={styles.payrollpage}>
-        <header>Payroll Viewer</header>
-        <section className={styles.viewercontainer}>
-          <PayrollTable />
-        </section>
-      </div>
-    </>
-  )
-}
+const PayrollPage: FC = () => (
+  <>
+    <Link to="/" className={styles.backbutton}>
+      <span>{'<'}</span>
+    </Link>
+    <div className={styles.payrollpage}>
+      <header>Payroll Viewer</header>
+      <section className={styles.viewercontainer}>
+        <PayrollTable />
+      </section>
+    </div>
+  </>
+)
 
 export default PayrollPage
